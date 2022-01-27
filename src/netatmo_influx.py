0b3651f2f877f7e30371d28e6f93e166d7ef0bd6
karakTaka <karaktaka@users.noreply.github.com>
2022-01-27 13:17:11 +0100

    Logging
    Add better logging and exception handling

--- src/netatmo_influx.py
+++ src/netatmo_influx.py
@@ -2,12 +2,14 @@
 # encoding=utf-8
 
 import signal
+import logging
 import argparse
 import configparser
 from time import sleep
 from os import environ
 from pathlib import Path
 from datetime import datetime
+from requests import ConnectionError
 from influxdb_client import InfluxDBClient, WritePrecision
 from pyatmo import ClientAuth, WeatherStationData, ApiError
 
@@ -27,8 +29,20 @@ def parse_config(config_file=None):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", dest="config", type=str, nargs=1, required=False)
+    parser.add_argument("-v", "--verbose", dest="verbosity", action="count", default=0)
 
     return parser.parse_args()
+
+
+def set_logging_level(verbosity):
+    switcher = {
+        0: logging.ERROR,
+        1: logging.WARNING,
+        2: logging.INFO,
+        3: logging.DEBUG,
+    }
+    loglevel = switcher.get(verbosity)
+    logging.basicConfig(format="%(asctime)s - %(levelname)s:%(message)s", datefmt="%d.%m.%Y %H:%M:%S", level=loglevel)
 
 
 def shutdown(_signal):
@@ -52,6 +66,9 @@ if __name__ == "__main__":
     influx_org = None
     args = parse_args()
     config = parse_config(args.config)
+
+    # set logging level
+    set_logging_level(args.verbosity)
 
     if environ.get("TERM"):
         signal.signal(signal.SIGTERM, shutdown)
@@ -121,79 +138,93 @@ if __name__ == "__main__":
                 scope="read_station",
             )
         except ApiError:
-            print("No credentials supplied. No Netatmo Account available.")
+            logging.error("No credentials supplied. No Netatmo Account available.")
             exit(1)
-
-        try:
-            weatherData = WeatherStationData(authorization)
-            weatherData.update()
-
-            with InfluxDBClient(
-                url=f"{influx_protocol}://{influx_host}:{influx_port}", token=influx_token, org=influx_org, debug=False
-            ) as _client:
-                with _client.write_api() as _write_client:
-
-                    for station_id in weatherData.stations:
-                        station_data = []
-                        module_data = []
-
-                        station = weatherData.get_station(station_id)
-                        station_name = station["station_name"]
-                        station_module_name = station["module_name"]
-
-                        altitude = station["place"]["altitude"]
-                        country = station["place"]["country"]
-                        timezone = station["place"]["timezone"]
-                        longitude = station["place"]["location"][0]
-                        latitude = station["place"]["location"][1]
-
-                        for module_id, moduleData in weatherData.get_last_data(station_id).items():
-                            module = weatherData.get_module(module_id)
-                            module_name = module["module_name"] if module else station["module_name"]
-
-                            if not module:
-                                for measurement in ["altitude", "country", "longitude", "latitude", "timezone"]:
-                                    value = eval(measurement)
-                                    if type(value) == int:
-                                        value = float(value)
-                                    station_data.append(
-                                        {
-                                            "measurement": measurement,
-                                            "tags": {"station": station_name, "module": module_name},
-                                            "fields": {"value": value},
-                                            "time": moduleData["When"],
-                                        }
-                                    )
-
-                            for sensor, value in moduleData.items():
-                                if sensor.lower() == "wifi_status":
-                                    sensor = "rf_status"
-                                if sensor.lower() != "when":
-                                    if type(value) == int:
-                                        value = float(value)
-                                    module_data.append(
-                                        {
-                                            "measurement": sensor.lower(),
-                                            "tags": {"station": station_name, "module": module_name},
-                                            "fields": {"value": value},
-                                            "time": moduleData["When"],
-                                        }
-                                    )
-
-                        now = datetime.utcnow()
-                        strtime = now.strftime("%Y-%m-%d %H:%M:%S")
-
-                        if _write_client.write(
-                            influx_bucket, influx_org, station_data, write_precision=WritePrecision.S
-                        ):
-                            print(f"({strtime}) Stations: {len(station_data)} Data Points written to Influxdb")
-
-                        if _write_client.write(
-                            influx_bucket, influx_org, module_data, write_precision=WritePrecision.S
-                        ):
-                            print(f"({strtime}) Modules: {len(module_data)} Data Points written to Influxdb")
-        except ApiError as error:
-            print(error)
+        except ConnectionError:
+            logging.error(f"Can't connect to Netatmo API. Retrying in {interval} second(s)...")
             pass
+        except KeyboardInterrupt:
+            logging.info("Received Interrupt. Shutting down...")
+            running = False
+        else:
+            try:
+                weatherData = WeatherStationData(authorization)
+                weatherData.update()
+
+                with InfluxDBClient(
+                    url=f"{influx_protocol}://{influx_host}:{influx_port}",
+                    token=influx_token,
+                    org=influx_org,
+                    debug=False,
+                ) as _client:
+                    with _client.write_api() as _write_client:
+
+                        for station_id in weatherData.stations:
+                            station_data = []
+                            module_data = []
+
+                            station = weatherData.get_station(station_id)
+                            station_name = station["station_name"]
+                            station_module_name = station["module_name"]
+
+                            altitude = station["place"]["altitude"]
+                            country = station["place"]["country"]
+                            timezone = station["place"]["timezone"]
+                            longitude = station["place"]["location"][0]
+                            latitude = station["place"]["location"][1]
+
+                            for module_id, moduleData in weatherData.get_last_data(station_id).items():
+                                module = weatherData.get_module(module_id)
+                                module_name = module["module_name"] if module else station["module_name"]
+
+                                if not module:
+                                    for measurement in ["altitude", "country", "longitude", "latitude", "timezone"]:
+                                        value = eval(measurement)
+                                        if type(value) == int:
+                                            value = float(value)
+                                        station_data.append(
+                                            {
+                                                "measurement": measurement,
+                                                "tags": {"station": station_name, "module": module_name},
+                                                "fields": {"value": value},
+                                                "time": moduleData["When"],
+                                            }
+                                        )
+
+                                for sensor, value in moduleData.items():
+                                    if sensor.lower() == "wifi_status":
+                                        sensor = "rf_status"
+                                    if sensor.lower() != "when":
+                                        if type(value) == int:
+                                            value = float(value)
+                                        module_data.append(
+                                            {
+                                                "measurement": sensor.lower(),
+                                                "tags": {"station": station_name, "module": module_name},
+                                                "fields": {"value": value},
+                                                "time": moduleData["When"],
+                                            }
+                                        )
+
+                            now = datetime.utcnow()
+                            strtime = now.strftime("%Y-%m-%d %H:%M:%S")
+
+                            if _write_client.write(
+                                influx_bucket, influx_org, station_data, write_precision=WritePrecision.S
+                            ):
+                                logging.info(
+                                    f"({strtime}) Stations: {len(station_data)} Data Points written to Influxdb"
+                                )
+
+                            if _write_client.write(
+                                influx_bucket, influx_org, module_data, write_precision=WritePrecision.S
+                            ):
+                                logging.info(f"({strtime}) Modules: {len(module_data)} Data Points written to Influxdb")
+            except ApiError as error:
+                logging.error(error)
+                pass
+            except KeyboardInterrupt:
+                logging.info("Received Interrupt. Shutting down...")
+                running = False
 
         sleep(interval)
